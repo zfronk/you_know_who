7982d21a5b263873bfa57513a6de86ab319517d2
Steve Aboll <officialstevve@gmail.com>
2025-01-28 23:53:09 +0300

    Fourth Commit

--- Program.cs
+++ Program.cs
@@ -18,12 +18,11 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
+app.UseStaticFiles();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+    
 
 app.Run();
